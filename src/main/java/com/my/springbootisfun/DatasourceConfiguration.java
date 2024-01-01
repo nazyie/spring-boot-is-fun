b0725eb13fa3dbc1ef826ab5fd2b265873212847
nazyie <nazyie.d@gmail.com>
2024-01-01 22:20:53 +0800

    [Configuration] Learn how to connect two db at the same time

--- src/main/java/com/my/springbootisfun/DatasourceConfiguration.java
+++ src/main/java/com/my/springbootisfun/DatasourceConfiguration.java
@@ -1,8 +1,12 @@
 package com.my.springbootisfun;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
@@ -28,5 +32,24 @@ public class DatasourceConfiguration {
         System.out.println("Data source for the PROD environment being loaded");
         System.out.println("=================================================");
         return "testing only";
+    }
+
+    /**
+     * Configuration (Connection for two database in single spring boot application)
+     * =============================================================================
+     * - Let said we have two DB instance to be connected
+     * - We can do this by setup the @Primary
+     */
+    @Primary
+    @Bean(name = "primaryDataSource")
+    @ConfigurationProperties(prefix = "primary.spring.datasource")
+    public DataSource dataSourceV1(DataSourceProperties dataSourceProperties){
+        return dataSourceProperties.initializeDataSourceBuilder().type(DriverManagerDataSource.class).build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @ConfigurationProperties(prefix = "secondary.spring.datasource")
+    public DataSource dataSourceV2(DataSourceProperties dataSourceProperties){
+        return dataSourceProperties.initializeDataSourceBuilder().type(DriverManagerDataSource.class).build();
     }
 }

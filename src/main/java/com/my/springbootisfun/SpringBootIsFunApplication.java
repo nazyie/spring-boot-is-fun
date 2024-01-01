4b15bd4df5a89ecc214b84358e74bb76360ac8b6
nazyie <nazyie.d@gmail.com>
2024-01-01 20:27:48 +0800

    Project initialization

---
+++ src/main/java/com/my/springbootisfun/SpringBootIsFunApplication.java
@@ -0,0 +1,13 @@
+package com.my.springbootisfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIsFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIsFunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/springbootisfun/SpringBootIsFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.springbootisfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIsFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

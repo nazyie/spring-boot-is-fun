9dacb8cb676580f7e01d82ba5c2bbcc03966078c
nazyie <nazyie.d@gmail.com>
2024-01-01 23:32:40 +0800

    [Dependencies Injection] learn the dependencies injection

--- src/main/java/com/my/springbootisfun/user/controller/UserController.java
+++ src/main/java/com/my/springbootisfun/user/controller/UserController.java
@@ -12,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-
+    /**
+     * Dependencies Injection (DI)
+     * ===========================
+     * - As we register our bean in the IOC
+     * - We can apply DI thru two ways
+     * - one is thru the @autowired or constructor class as below
+     */
     @Autowired
     private UserService service;
+
+//    public usercontroller(userservice userservice){
+//        this.service = userservice;
+//    }
 
     @GetMapping
     public User getUser(){
